181d9ef1cfed2bec054bd7fdfc52f7f0da368940
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-22 09:07:18 +0300

    Now with passing parameters to templates!

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # return HttpResponse("Hello, World")
     return render(request, "hello/index.html")
 
 def mads(request):
     return HttpResponse("Hello, Mads!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    # return HttpResponse(f"Hello, {name.capitalize()}")
+    context = {
+         "name": name.capitalize()
+    }
+    
+    return render(request, "hello/greet.html", context)
