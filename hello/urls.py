208d1a72061c845a76df7a41b26072db8307c8a2
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-22 08:58:40 +0300

    Now with html templates

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hello, name="hello"),
+    path("", views.index, name="index"),
      path("<str:name>", views.greet, name="greet"), # We execute views.greet function whenever we type/hello/ "any name"
     path("mads", views.mads, name="mads")
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello")
+def index(request):
+    return render(request, "hello/index.html")
 
 def mads(request):
     return HttpResponse("Hello, Mads!")
