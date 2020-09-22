ec4bd3cde04b64059f4d6de71a38baf78632afff
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-22 12:26:21 +0300

    Now with: - Tasks App - Free frontend validation - Form Templates - JINJA dynamic links -.. and more

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,22 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+tasks = ["Do groceries", "Buy some soap", "Clean the dishes"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+def add(request):
+    if request.method == "POST":
+        return render(request, "tasks/index.html")
+        # Grab the data and do things with it
+    else:
+        return render(request, "tasks/add.html", {
+            "form": NewTaskForm()
+        })
