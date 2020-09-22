947960c162f2dd65bf685373f884b2b9d4350674
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-22 13:06:03 +0300

    Now with free client side and server side FORM validation!

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ tasks = ["Do groceries", "Buy some soap", "Clean the dishes"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+    # Client side FORM validation - for free!
     priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request):
@@ -14,9 +15,20 @@ def index(request):
     })
 def add(request):
     if request.method == "POST":
-        return render(request, "tasks/index.html")
-        # Grab the data and do things with it
+
+        # Server side FORM validation - for free!
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            # The valid, clean data is found in this form.cleaned_data var
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+            # Actualy send back the same form data on deny, that`s pretty awesome!
     else:
+        # GET : Display the "Add New Task" form
         return render(request, "tasks/add.html", {
             "form": NewTaskForm()
         })
